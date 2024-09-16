7c089c9d37217c3b0453b7013ec272dfc3c6b301
anto <above@gualaclosures.com>
2024-09-16 22:49:14 +0200

    Rename template to actual app name
    replace-in-file doesn't seem to work, foung nice cli: ruplace

--- sst.config.ts
+++ sst.config.ts
@@ -3,7 +3,7 @@
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
+      name: "sst-test-app",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
     };
